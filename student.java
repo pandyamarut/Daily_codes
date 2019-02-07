470f5198481d4b1141331379143f4f37ab67f65c
pandyamarut <pandyamarut@gmail.com>
2019-02-07 19:13:19 +0530

    An attempt to perform mathematical operation

--- student.java
+++ student.java
@@ -1,9 +1,9 @@
 import java.io.*;
 class student
 {
-public void Name()
-{
-System.out.println("name : varun");
+public int Name(int a, int b)
+{ 
+return (a+b);
 }
 public void USN()
 {
@@ -12,7 +12,8 @@ System.out.println("Usn:66");
 public static void main(String args[])
 {
 student s = new student();
-s.Name();
+s.Name(10,20);
 s.USN();
+System.out.println("Name class not working pleasae do check");
 }
 }
