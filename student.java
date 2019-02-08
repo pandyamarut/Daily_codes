d0ca9cbdda988533e026caad18a03ca395a5bf84
pandyamarut <pandyamarut@gmail.com>
2019-02-08 19:42:39 +0530

    added function

--- student.java
+++ student.java
@@ -9,11 +9,16 @@ public void USN()
 {
 System.out.println("Usn:66");
 }
+public void College()
+{
+System.out.println("DSCE");
+}
 public static void main(String args[])
 {
 student s = new student();
 s.Name(10,20);
 s.USN();
-System.out.println("Name class not working pleasae do check");
+s.College();
+System.out.println("Name class not working please do check");
 }
 }
