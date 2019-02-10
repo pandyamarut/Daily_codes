8b7b2dc9a167de71ad5a70c06c9999972ed39990
pandyamarut <pandyamarut@gmail.com>
2019-02-10 15:17:29 +0530

    trying to print Sequence of N numbers without using loop.

---
+++ sequence.py
@@ -0,0 +1,10 @@
+T = int(input())
+for i in range(T):
+    i=int(input())
+    lst=[];
+    lst.append(i)
+    if lst[]>0:
+        lst[i+1]=lst[i]-5
+    else:
+        lst[i+1]=lst[i]+5
+    print(lst)   
